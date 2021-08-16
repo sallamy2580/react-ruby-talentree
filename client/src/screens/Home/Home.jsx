86b49ecf5627127a5c29dfee4d5932fa51aa6744
Superpol@ris <sallay2580@gmail.com>
2021-08-16 12:18:20 -0700

    Add option to create new course for teachers

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { AiOutlinePlus } from "react-icons/ai";
+import { BsPencilSquare } from "react-icons/bs";
+import { HiOutlineUserGroup } from "react-icons/hi";
 import Post from "../../components/Post/Post";
 import { getPosts } from "../../services/posts";
 import "./Home.css";
 const Home = ({ userData }) => {
   const [posts, setPosts] = useState([]);
   const [toggleFetch, setToggleFetch] = useState(true);
+  const [showOptions, setShowOptions] = useState(false);
   useEffect(() => {
     const fetchPosts = async () => {
       const data = await getPosts();
@@ -14,6 +17,10 @@ const Home = ({ userData }) => {
     };
     fetchPosts();
   }, [toggleFetch]);
+
+  const handleClick = () => {
+    setShowOptions(!showOptions);
+  };
 
   return (
     <main id="home">
@@ -27,9 +34,32 @@ const Home = ({ userData }) => {
           />
         ))}
       </section>
-      <Link className="new-post-link" to="/new">
-        <AiOutlinePlus />
-      </Link>
+      {userData.is_teacher ? (
+        <button className="new-post-link" onClick={handleClick}>
+          <AiOutlinePlus />
+        </button>
+      ) : (
+        <Link className="new-post-link" to="/new">
+          <AiOutlinePlus />
+        </Link>
+      )}
+      <div className={`show-options${showOptions ? " show" : ""}`}>
+        <div className="options-box">
+          <div className="options-links">
+            <Link to="/new">
+              {" "}
+              <HiOutlineUserGroup /> Class
+            </Link>
+            <Link to="/new">
+              {" "}
+              <BsPencilSquare /> Post
+            </Link>
+          </div>
+          <button className="cancel-button" onClick={handleClick}>
+            Cancel
+          </button>
+        </div>
+      </div>
     </main>
   );
 };
